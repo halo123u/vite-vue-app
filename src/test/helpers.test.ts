41eb8055410f63d667796c370aea687337e10776
oswaldo almazo <oswaldo.almazo@gmail.com>
2022-10-16 15:30:10 -0400

    Add vitest to project and create tests for helper.ts file.

---
+++ src/test/helpers.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect} from 'vitest';
+import { generateUsers, toUpperCase } from '../helpers';
+describe('helpers suite', () => {
+  it('should generate empty array', () => {
+    const users = generateUsers(0);
+
+    expect(users).toEqual([]);
+  })
+  it('should generate 1 user', () => {
+    const users = generateUsers(1);
+
+    expect(users).toEqual([{
+      "id": 1,
+      "username": "User 0",
+    }]);
+  })
+  it('should convert string to upper case', () => {
+    expect(toUpperCase('hello world')).toBe('HELLO WORLD')
+  })
+})
